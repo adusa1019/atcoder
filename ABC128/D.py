bd640787599fc08de4a3baa021a431b4b7bbd312
adusa1019 <symphony20030829@yahoo.co.jp>
2019-06-19 13:51:40 +0900

    refactor: fix duplicate func name

--- ABC128/D.py
+++ ABC128/D.py
@@ -1,7 +1,7 @@
 from itertools import accumulate
 
 
-def solve(string):
+def solve_easy(string):
     n, k, *v = map(int, string.split())
     ans = 0
     for i in range(k + 1):
@@ -14,16 +14,10 @@ def solve(string):
 def solve(string):
     n, k, *v = map(int, string.split())
     r = min(n, k)
-    """
-    for i in range(k + 1):
-        for j in range(k - i + 1):
-            base = sorted(v[:i] + v[max(n - k + i + j, i):])
-            ans = max(ans, sum([b for l, b in enumerate(base) if b >= 0 or j <= l]))
-    """
 
     def s(w):
         l = [0] * (k + 1)
-        for i in range(1, k + 1):
+        for i in range(1, r + 1):
             _w = sorted(w[:i], reverse=True)
             tmp = sum(_w)
             l[i] = max(l[i], tmp)
