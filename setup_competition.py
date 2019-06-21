f7445e9e10f2ee01d07b3bdcff4a9540d22a64b8
adusa1019 <symphony20030829@yahoo.co.jp>
2019-06-21 13:32:50 +0900

    feat: use beautifulsoup

--- setup_competition.py
+++ setup_competition.py
@@ -1,33 +1,44 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
 import argparse
 import os
 
 import requests
+from bs4 import BeautifulSoup as bs
+from bs4.element import Comment
+
+import config
 
 
-def get_ins_outs(response):
-    texts = response.text.split("\r\n")
-    starts = [i for i, _l in enumerate(texts) if "<pre>" in _l]
-    ends = [i for i, _l in enumerate(texts) if "</pre>" in _l]
-
-    tmp = []
-    for _s, _e in zip(starts, ends):
-        if "Sample" not in texts[_s]:
-            continue
-        tmp.append("\n".join(texts[_s:_e]).split("<pre>")[-1])
-    return tmp
+def login(session):
+    LOGIN_URL = "https://atcoder.jp/login"
+    r = session.get(LOGIN_URL)
+    csrf_token = bs(r.text, 'lxml').find(attrs={'name': 'csrf_token'}).get('value')
+    login_info = {"csrf_token": csrf_token, "username": config.name, "password": config.pwd}
+    session.post(LOGIN_URL, data=login_info)
 
 
-def make_test_code(_file, problem, _in, _out):
-    with open(_file, "w") as f:
+def get_urls(result):
+    urls = [
+        b.a for b in bs(result.text, "html.parser").find_all("td", class_="text-center no-break")
+    ]
+    return [(b.text, base + b.attrs["href"].split("/")[-1]) for b in urls]
+
+
+def get_ins_outs(result):
+    return [
+        b.text.strip()
+        for b in bs(result.text, "html.parser").find_all("pre")
+        if len(b.contents) == 1 and tag_visible(b)
+    ]
+
+
+def make_test_code(savedir, problem, inout):
+    with open("{}/Test_{}.py".format(savedir, problem), "w") as f:
         f.write("import pytest\n")
         f.write("from {} import solve\n\n\n".format(problem.upper()))
         f.write("def test_solve():\n")
-        for _i, _o in zip(_in, _out):
-            f.write("    assert solve('{}') == '{}'\n".format(_i.replace("\n", "\\n"),
-                                                              _o.replace("\n", "\\n")))
+        for _i, _o in zip(*[iter(inout)] * 2):
+            f.write("    assert solve('{}') == '{}'\n".format("\\n".join(_i.splitlines()),
+                                                              "\\n".join(_o.splitlines())))
 
 
 def make_answer_basecode(_file):
@@ -38,33 +49,27 @@ def make_answer_basecode(_file):
         f.write("    print(solve(input()))\n")
 
 
+def tag_visible(element):
+    if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
+        return False
+    if isinstance(element, Comment):
+        return False
+    return True
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("number", type=int)
     parser.add_argument("--division", choices=["abc", "arc"], default="abc")
     args = parser.parse_args()
 
-    prob_char = ["a", "b", "c", "d"] if args.number < 126 else ["a", "b", "c", "d", "e", "f"]
-    base = "https://{div}{num:03}.contest.atcoder.jp/".format(div=args.division, num=args.number)
-    r = requests.get(base + "assignments/")
-    tmp = [_l for _l in r.text.split("\n") if "linkwrapper" in _l.strip()][::2]
-    urls = [_t.split("href=\"")[1].split("\">")[0] for _t in tmp]
-
-    for _c, _u in zip(prob_char, urls):
-        save_dir = "{}{:03}".format(args.division.upper(), args.number)
-        if not os.path.exists(save_dir):
-            os.mkdir(save_dir)
-
-        r = requests.get(base + _u)
-        ins_outs = get_ins_outs(r)
-        ins = ins_outs[::2]
-        outs = ins_outs[1::2]
-        print(ins)
-        print(outs)
-
-        testcode = "{}/Test_{}.py".format(save_dir, _c.upper())
-        make_test_code(testcode, _c, ins, outs)
-        answercode = "{}/{}.py".format(save_dir, _c.upper())
-        if os.path.exists(answercode):
-            continue
-        make_answer_basecode(answercode)
+    save_dir = "{}{:03}".format(args.division.upper(), args.number)
+    if not os.path.exists(save_dir):
+        os.mkdir(save_dir)
+    base = "https://atcoder.jp/contests/{}{:03}/tasks/".format(args.division, args.number)
+    with requests.session() as s:
+        login(s)
+        urls = get_urls(s.get(base))
+        for name, url in urls:
+            make_answer_basecode("{}/{}.py".format(save_dir, name))
+            make_test_code(save_dir, name, get_ins_outs(s.get(url)))
