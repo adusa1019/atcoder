861f9589b56e1373a12de92e5c37fd1fa130b9a2
adusa1019 <symphony20030829@yahoo.co.jp>
2019-08-07 16:17:52 +0900

    refactor: DRY

--- ABC136/D.py
+++ ABC136/D.py
@@ -1,24 +1,20 @@
 def solve(string):
     n = len(string)
-    ans = [0] * n
-    count = 0
-    for i, a in enumerate(string):
-        if a == "R":
-            count += 1
-            continue
-        if count > 0:
-            ans[i] += count // 2
-            ans[i - 1] += (count + 1) // 2
-            count = 0
-    for i, a in enumerate(string[::-1]):
-        if a == "L":
-            count += 1
-            continue
-        if count > 0:
-            ans[n - i - 1] += count // 2
-            ans[n - i] += (count + 1) // 2
-            count = 0
-    return " ".join([str(a) for a in ans])
+
+    def op(string, ans, forward=True):
+        count = 0
+        check, sign, base = ("R", 1, 0) if forward else ("L", -1, n - 1)
+        for i, a in enumerate(string[::sign]):
+            if a == check:
+                count += 1
+            elif count > 0:
+                ans[base + sign * i] += count // 2
+                ans[base + sign * (i - 1)] += (count + 1) // 2
+                count = 0
+        return ans
+
+    ans = op(string, [0] * n, True)
+    return " ".join([str(a) for a in op(string, ans, False)])
 
 
 if __name__ == '__main__':
