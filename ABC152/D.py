6de524921d66a3a58f98280f0107673b27d9f43c
adusa1019 <symphony20030829@yahoo.co.jp>
2020-01-27 06:14:40 +0000

    add faster solve

--- ABC152/D.py
+++ ABC152/D.py
@@ -10,6 +10,21 @@ def solve(string):
     return str(sum(t[i][j] * t[j][i] for i, j in product(range(1, 10), repeat=2)))
 
 
+def faster_solve(string):
+    d = len(string)
+    if d == 1:
+        return string
+    s0, ss, sn = int(string[0]), int(string[1:-1] or 0), int(string[-1])
+    t = [[0] * 10] + [[int("1" * (d - 2) or 0)] * 10 for _ in range(9)]
+    for i in range(1, 10):
+        t[i][i] += 1
+    for i, j in product(range(1, s0), range(10)):
+        t[i][j] += 10**(d - 2)
+    for j in range(10):
+        t[s0][j] += ss + 1 if j <= sn else ss
+    return str(sum(t[i][j] * t[j][i] for i, j in product(range(1, 10), repeat=2)))
+
+
 if __name__ == '__main__':
     import sys
     print(solve(sys.stdin.read().strip()))
