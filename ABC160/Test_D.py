7aa9338acc1a743289b5a7404ead7fcc335e39dd
adusa1019 <symphony20030829@yahoo.co.jp>
2020-04-03 05:39:44 +0000

    solve: ABC160 A,B,C,D

--- ABC160/Test_D.py
+++ ABC160/Test_D.py
@@ -3,7 +3,7 @@ from D import solve
 
 
 def test_solve():
-    # assert solve('5 2 4') == '5\n4\n1\n0'
-    # assert solve('3 1 3') == '3\n0'
+    assert solve('5 2 4') == '5\n4\n1\n0'
+    assert solve('3 1 3') == '3\n0'
     assert solve('7 3 7') == '7\n8\n4\n2\n0\n0'
     assert solve('10 4 8') == '10\n12\n10\n8\n4\n1\n0\n0\n0'
